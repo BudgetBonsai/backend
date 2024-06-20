456cf0438c20e0d69818ae4be03f813764c1b985
Muhammad Solikhin <school.muhammad.solikhin@gmail.com>
2024-06-20 15:58:42 +0700

    update

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,7 +1,7 @@
 import * as tf from '@tensorflow/tfjs-node';
 
 async function loadModel() {
-    return tf.loadLayersModel(process.env.MODEL_URL);
+   // return tf.loadLayersModel(process.env.MODEL_URL);
 }
 
 export default loadModel;
